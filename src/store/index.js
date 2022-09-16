fd8df3d9bbdf7850142dd9e8a8d8e0654497c566
Roman Grin <grinromchik@gmail.com>
2022-09-16 19:34:31 +0300

    enhancer for store self implementation

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,23 @@
 import { createStore, combineReducers } from 'redux';
 import filters from '../reducers/filters';
 import heroes from '../reducers/heroes';
-const store = createStore(
-    combineReducers({ filters, heroes }),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
 
+const enhancer =
+    (createStore) =>
+    (...args) => {
+        const store = createStore(...args);
+
+        const oldDispatch = store.dispatch; // saving old store
+        store.dispatch = (action) => {
+            // осталеживем нашу строку в диспетч и сами формируем объект
+            if (typeof action === 'string') {
+                return oldDispatch({ type: action });
+            }
+            return oldDispatch(action);
+        };
+        return store;
+    };
+
+const store = createStore(combineReducers({ filters, heroes }), enhancer);
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 export default store;
